83f626dec32c698d1c40f72421a4c74e3895697a
ichiragkumar <chiragroy3030@gmail.com>
2024-06-08 15:59:05 +0530

    dburl added & connection pool url added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,35 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/signup', (c) => {
+  return c.text('signup route')
+})
+
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin route')
+})
+
+
+app.post('/api/v1/blog', (c) => {
+  return  c.text('Blog Added')
+})
+
+
+app.put('/api/v1/blog', (c) =>{
+  return  c.text('Blog Updated')
+})
+
+
+app.get("/api/v1/blog/:id", (c)=> {
+  return  c.text("single Blog")
+})
+
+
+
+app.get("/api/v1/blog/bulk", (c)=> {
+  return c.text("Blogs in Bulk")
+})
+
+
 export default app
